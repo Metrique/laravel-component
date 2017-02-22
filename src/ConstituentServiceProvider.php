1076223adba6ad3c1f46bd93af9a15852e43cd2a
Daniel Hudson <danielhudson@shinobicorp.com>
2017-02-22 23:57:54 +0000

    Bind constituent to service container

--- src/ConstituentServiceProvider.php
+++ src/ConstituentServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Metrique\Constituent;
 
 use Illuminate\Support\ServiceProvider;
+use Metrique\Constituent\Constituent;
+use Metrique\Constituent\ConstituentInterface;
 use Metrique\Constituent\ConstituentViewComposer;
 
 class ConstituentServiceProvider extends ServiceProvider
@@ -25,7 +27,7 @@ class ConstituentServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(ConstituentInterface::class, Constituent::class);
     }
 
     protected function bootBladeDirective()
