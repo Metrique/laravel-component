f71b12dbb2b0645d3484daba75c51cfef1527428
Daniel Hudson <danielhudson@shinobicorp.com>
2019-09-11 14:22:27 +0100

    Update Strings.php

--- src/Traits/Strings.php
+++ src/Traits/Strings.php
@@ -2,15 +2,17 @@
 
 namespace Metrique\Constituent\Traits;
 
+use Illuminate\Support\Str;
+
 trait Strings
 {
     public function toTitleCase(string $string)
     {
-        $string = snake_case($string);
+        $string = Str::snake($string);
         $string = str_replace('_', ' ', $string);
         $string = ucfirst($string);
         $string = preg_replace('/(\S)([\d-]+)/', '$1 $2', $string);
-        
+
         return $string;
     }
 }
